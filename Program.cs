e9bf820fc7a04fbecebb26c126695ce042cac064
Markus von Arx <mkvonarx@gmail.com>
2020-12-12 23:06:47 +0100

    print XmpMetaFactory.SchemaRegistry for more information

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using MetadataExtractor;
 using MetadataExtractor.Formats.Xmp;
+using XmpCore;
 
 namespace MetadataExtractorBug
 {
@@ -10,25 +11,75 @@ namespace MetadataExtractorBug
 	{
 		static void Main(string[] args)
 		{
+			//RegisterNamespaceTest();
+
+			PrintSchemaRegistry();
+			Console.WriteLine();
+
 			foreach (var filePath in args)
 			{
 				if (File.Exists(filePath))
 				{
 					Console.WriteLine($"\"{filePath}\":");
+					//XmpMetaFactory.Reset(); // calling Reset() fixes the issue with Namespaces and Path being "MicrosoftPhoto:Rating" or "MicrosoftPhoto_1_:Rating", but Reset() should not be called as per the documentation ...
 					var metadataDirectories = ImageMetadataReader.ReadMetadata(filePath);
 					var xmpDirectory = metadataDirectories.OfType<XmpDirectory>().FirstOrDefault();
 					foreach (var property in xmpDirectory.XmpMeta.Properties)
 					{
-						if (((property.Path != null) && property.Path.Contains("MicrosoftPhoto", StringComparison.OrdinalIgnoreCase)) ||
-						    ((property.Namespace != null) && property.Namespace.Contains("ns.microsoft.com/photo/1.0", StringComparison.OrdinalIgnoreCase)))
+						if (((property.Path != null) && property.Path.Contains("microsoft" /*"MicrosoftPhoto"*/, StringComparison.OrdinalIgnoreCase)) ||
+						    ((property.Namespace != null) && property.Namespace.Contains("microsoft" /*"ns.microsoft.com/photo/1.0"*/, StringComparison.OrdinalIgnoreCase)))
 						{
 							Console.WriteLine($"  XMP: path={property.Path} => value={property.Value}, namespace={property.Namespace}");
 						}
 					}
+					PrintSchemaRegistry();
 				}
 				else
 				{
 					Console.WriteLine($"ERROR: file \"{filePath}\" not found");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		private static void RegisterNamespaceTest()
+		{
+			Console.WriteLine("RegisterNamespace(\"http://ns.microsoft.com/photo/1.0/\", \"MicrosoftPhoto:\")");
+			var result1 = XmpMetaFactory.SchemaRegistry.RegisterNamespace("http://ns.microsoft.com/photo/1.0/", "MicrosoftPhoto:");
+			Console.WriteLine($"  -> returns \"{result1}\"");
+
+			Console.WriteLine("RegisterNamespace(\"http://ns.microsoft.com/photo/1.0\", \"MicrosoftPhoto:\")");
+			var result2 = XmpMetaFactory.SchemaRegistry.RegisterNamespace("http://ns.microsoft.com/photo/1.0", "MicrosoftPhoto:");
+			Console.WriteLine($"  -> returns \"{result2}\"");
+		
+			Console.WriteLine();
+		}
+
+		private static void PrintSchemaRegistry()
+		{
+			Console.WriteLine("SchemaRegistry:");
+
+			//Console.WriteLine($"  {XmpMetaFactory.SchemaRegistry.Aliases.Count} Aliases:"); // always the same 35
+			//foreach (var alias in XmpMetaFactory.SchemaRegistry.Aliases)
+			//{
+			//	Console.WriteLine($"    {alias.Key} = {alias.Value}");
+			//}
+
+			Console.WriteLine($"  {XmpMetaFactory.SchemaRegistry.Namespaces.Count} Namespaces, printing the ones containing microsoft:");
+			foreach (var ns in XmpMetaFactory.SchemaRegistry.Namespaces)
+			{
+				if (ns.Key.Contains("microsoft", StringComparison.OrdinalIgnoreCase) || ns.Value.Contains("microsoft", StringComparison.OrdinalIgnoreCase))
+				{
+					Console.WriteLine($"    namespace:{ns.Key} => prefix:{ns.Value}");
+				}
+			}
+
+			Console.WriteLine($"  {XmpMetaFactory.SchemaRegistry.Prefixes.Count} Prefixes, printing the ones containing microsoft:");
+			foreach (var pre in XmpMetaFactory.SchemaRegistry.Prefixes)
+			{
+				if (pre.Key.Contains("microsoft", StringComparison.OrdinalIgnoreCase) || pre.Value.Contains("microsoft", StringComparison.OrdinalIgnoreCase))
+				{
+					Console.WriteLine($"    prefix:{pre.Key} => namespace:{pre.Value}");
 				}
 			}
 		}
--- run_tests.bat
+++ run_tests.bat
@@ -19,45 +19,100 @@ goto :eof
 results:
 
 $ .\bin\Debug\net5.0\MetadataExtractorBug.exe Test-exiftool.jpg
+SchemaRegistry:
+  57 Namespaces, printing the ones containing microsoft:
+  57 Prefixes, printing the ones containing microsoft:
+
 "Test-exiftool.jpg":
   XMP: path= => value=, namespace=http://ns.microsoft.com/photo/1.0
   XMP: path=MicrosoftPhoto:LastKeywordXMP => value=, namespace=http://ns.microsoft.com/photo/1.0
   XMP: path=MicrosoftPhoto:LastKeywordXMP[1] => value=exiftool-MSKeyword1, namespace=
   XMP: path=MicrosoftPhoto:LastKeywordXMP[2] => value=exiftool-MSKeyword2, namespace=
   XMP: path=MicrosoftPhoto:Rating => value=25, namespace=http://ns.microsoft.com/photo/1.0
+SchemaRegistry:
+  58 Namespaces, printing the ones containing microsoft:
+    namespace:http://ns.microsoft.com/photo/1.0 => prefix:MicrosoftPhoto:
+  58 Prefixes, printing the ones containing microsoft:
+    prefix:MicrosoftPhoto: => namespace:http://ns.microsoft.com/photo/1.0
+
 
 $ .\bin\Debug\net5.0\MetadataExtractorBug.exe Test-WindowsExplorer.jpg
+SchemaRegistry:
+  57 Namespaces, printing the ones containing microsoft:
+  57 Prefixes, printing the ones containing microsoft:
+
 "Test-WindowsExplorer.jpg":
   XMP: path= => value=, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto:Rating => value=75, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto:LastKeywordXMP => value=, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto:LastKeywordXMP[1] => value=WinExplorerTag1, namespace=
   XMP: path=MicrosoftPhoto:LastKeywordXMP[2] => value=WinExplorerTag2, namespace=
+SchemaRegistry:
+  58 Namespaces, printing the ones containing microsoft:
+    namespace:http://ns.microsoft.com/photo/1.0/ => prefix:MicrosoftPhoto:
+  58 Prefixes, printing the ones containing microsoft:
+    prefix:MicrosoftPhoto: => namespace:http://ns.microsoft.com/photo/1.0/
+
 
 $ .\bin\Debug\net5.0\MetadataExtractorBug.exe Test-exiftool.jpg Test-WindowsExplorer.jpg
+SchemaRegistry:
+  57 Namespaces, printing the ones containing microsoft:
+  57 Prefixes, printing the ones containing microsoft:
+
 "Test-exiftool.jpg":
   XMP: path= => value=, namespace=http://ns.microsoft.com/photo/1.0
   XMP: path=MicrosoftPhoto:LastKeywordXMP => value=, namespace=http://ns.microsoft.com/photo/1.0
   XMP: path=MicrosoftPhoto:LastKeywordXMP[1] => value=exiftool-MSKeyword1, namespace=
   XMP: path=MicrosoftPhoto:LastKeywordXMP[2] => value=exiftool-MSKeyword2, namespace=
   XMP: path=MicrosoftPhoto:Rating => value=25, namespace=http://ns.microsoft.com/photo/1.0
+SchemaRegistry:
+  58 Namespaces, printing the ones containing microsoft:
+    namespace:http://ns.microsoft.com/photo/1.0 => prefix:MicrosoftPhoto:
+  58 Prefixes, printing the ones containing microsoft:
+    prefix:MicrosoftPhoto: => namespace:http://ns.microsoft.com/photo/1.0
+
 "Test-WindowsExplorer.jpg":
   XMP: path= => value=, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto_1_:Rating => value=75, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto_1_:LastKeywordXMP => value=, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto_1_:LastKeywordXMP[1] => value=WinExplorerTag1, namespace=
   XMP: path=MicrosoftPhoto_1_:LastKeywordXMP[2] => value=WinExplorerTag2, namespace=
+SchemaRegistry:
+  59 Namespaces, printing the ones containing microsoft:
+    namespace:http://ns.microsoft.com/photo/1.0 => prefix:MicrosoftPhoto:
+    namespace:http://ns.microsoft.com/photo/1.0/ => prefix:MicrosoftPhoto_1_:
+  59 Prefixes, printing the ones containing microsoft:
+    prefix:MicrosoftPhoto: => namespace:http://ns.microsoft.com/photo/1.0
+    prefix:MicrosoftPhoto_1_: => namespace:http://ns.microsoft.com/photo/1.0/
+
 
 $ .\bin\Debug\net5.0\MetadataExtractorBug.exe Test-WindowsExplorer.jpg Test-exiftool.jpg
+SchemaRegistry:
+  57 Namespaces, printing the ones containing microsoft:
+  57 Prefixes, printing the ones containing microsoft:
+
 "Test-WindowsExplorer.jpg":
   XMP: path= => value=, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto:Rating => value=75, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto:LastKeywordXMP => value=, namespace=http://ns.microsoft.com/photo/1.0/
   XMP: path=MicrosoftPhoto:LastKeywordXMP[1] => value=WinExplorerTag1, namespace=
   XMP: path=MicrosoftPhoto:LastKeywordXMP[2] => value=WinExplorerTag2, namespace=
+SchemaRegistry:
+  58 Namespaces, printing the ones containing microsoft:
+    namespace:http://ns.microsoft.com/photo/1.0/ => prefix:MicrosoftPhoto:
+  58 Prefixes, printing the ones containing microsoft:
+    prefix:MicrosoftPhoto: => namespace:http://ns.microsoft.com/photo/1.0/
+
 "Test-exiftool.jpg":
   XMP: path= => value=, namespace=http://ns.microsoft.com/photo/1.0
   XMP: path=MicrosoftPhoto_1_:LastKeywordXMP => value=, namespace=http://ns.microsoft.com/photo/1.0
   XMP: path=MicrosoftPhoto_1_:LastKeywordXMP[1] => value=exiftool-MSKeyword1, namespace=
   XMP: path=MicrosoftPhoto_1_:LastKeywordXMP[2] => value=exiftool-MSKeyword2, namespace=
   XMP: path=MicrosoftPhoto_1_:Rating => value=25, namespace=http://ns.microsoft.com/photo/1.0
+SchemaRegistry:
+  59 Namespaces, printing the ones containing microsoft:
+    namespace:http://ns.microsoft.com/photo/1.0/ => prefix:MicrosoftPhoto:
+    namespace:http://ns.microsoft.com/photo/1.0 => prefix:MicrosoftPhoto_1_:
+  59 Prefixes, printing the ones containing microsoft:
+    prefix:MicrosoftPhoto: => namespace:http://ns.microsoft.com/photo/1.0/
+    prefix:MicrosoftPhoto_1_: => namespace:http://ns.microsoft.com/photo/1.0
